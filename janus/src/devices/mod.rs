f1cc0208a0fa339008ea3fcf307bcb6b99a91f60
rbmj <rbmj@verizon.net>
2023-10-17 08:04:19 -0700

    Osc shape fixes - still some frequency wandering

--- janus/src/devices/mod.rs
+++ janus/src/devices/mod.rs
@@ -4,6 +4,7 @@ use super::fixedmath;
 use super::fixedmath::Sample as SampleFxP;
 use super::fixedmath::USample as USampleFxP;
 use super::fixedmath::Note as NoteFxP;
+use super::fixedmath::Scalar as ScalarFxP;
 
 const STATIC_BUFFER_SIZE : usize = 256;
 type BufferT<T> = [T; STATIC_BUFFER_SIZE];
--- janus/src/devices/osc.rs
+++ janus/src/devices/osc.rs
@@ -51,7 +51,6 @@ impl OscillatorFxP {
         const SAMPLE_RATE : fixedmath::U16F0 = fixedmath::U16F0::lit("44100");
         // 4096*2*pi/44.1k
         const FRAC_4096_2PI_SR : fixedmath::U0F32 = fixedmath::U0F32::lit("0x0.9565925d");
-        const SHAPE_CAP : fixedmath::U0F16 = fixedmath::U0F16::lit("0x0.FF00");
         for i in 0..numsamples {
             //generate waveforms (piecewise defined)
             let frac_2phase_pi = SampleFxP::from_num(PhaseFxP_Trunc::from_num(
@@ -87,14 +86,15 @@ impl OscillatorFxP {
             let phase_per_sample = PhaseFxP::from_num(fixedmath::midi_note_to_frequency(note[i])
                 .wide_mul(FRAC_4096_2PI_SR)
                 .unwrapped_shr(12));
-            let shape_capped = if shape[i] <= SHAPE_CAP { shape[i] } else { SHAPE_CAP };
-            let mut adjustment = PhaseFxP::from_num(
-                fixedmath::U1F15::from_num(phase_per_sample).wide_mul(shape_capped));
-            if self.phase < PhaseFxP::ZERO {
-                adjustment = adjustment.unwrapped_neg();
+            let phase_per_smp_adj = if self.phase < PhaseFxP::ZERO {
+                PhaseFxP::from_num(phase_per_sample.wide_mul(fixedmath::I20F12::from_num(
+                    one_over_one_plus_x(shape[i]))))
             }
-            let phase_per_sample_adj = phase_per_sample + adjustment;
-            self.phase += phase_per_sample_adj;
+            else {
+                PhaseFxP::from_num(phase_per_sample.wide_mul(fixedmath::I20F12::from_num(
+                    one_over_one_minus_x(shape[i]))))
+            };
+            self.phase += phase_per_smp_adj;
             if self.phase >= PhaseFxP::PI {
                 self.phase -= PhaseFxP::TAU;
             }
@@ -104,6 +104,151 @@ impl OscillatorFxP {
             tri: &self.tribuf[0..numsamples],
             sq: &self.sqbuf[0..numsamples]
         }    
+    }
+}
+
+fn clip_shape(x: ScalarFxP) -> ScalarFxP {
+    const CLIP_MAX: ScalarFxP = ScalarFxP::lit("0x0.F");
+    if x > CLIP_MAX {
+        CLIP_MAX
+    }
+    else {
+        x
+    }
+}
+fn one_over_one_minus_x(x: ScalarFxP) -> USampleFxP {
+    let x_bits = clip_shape(x).to_bits();
+    // Table generated with python:
+    //
+    // table = [1/(1-(x/256.0)) for x in range(0,256)][:0xF1]
+    // shifted = [int(x*256*16) for x in table]
+    // shifted[-1] = shifted[-1] - 1 # Prevent overflow
+    // hexvals = [hex(x) for x in shifted]
+    // for i in range(len(hexvals)):
+    //     val = hexvals[i]
+    //     print('USampleFxP::lit("' + val[:3] + '.' + val[3:] + '"), ', end='')
+    //     if i % 4 == 3:
+    //         print('')
+    const LOOKUP_TABLE : [USampleFxP; 0xF2] = [
+        USampleFxP::lit("0x1.000"), USampleFxP::lit("0x1.010"), USampleFxP::lit("0x1.020"), USampleFxP::lit("0x1.030"),
+        USampleFxP::lit("0x1.041"), USampleFxP::lit("0x1.051"), USampleFxP::lit("0x1.062"), USampleFxP::lit("0x1.073"),
+        USampleFxP::lit("0x1.084"), USampleFxP::lit("0x1.095"), USampleFxP::lit("0x1.0a6"), USampleFxP::lit("0x1.0b7"),
+        USampleFxP::lit("0x1.0c9"), USampleFxP::lit("0x1.0db"), USampleFxP::lit("0x1.0ec"), USampleFxP::lit("0x1.0fe"),
+        USampleFxP::lit("0x1.111"), USampleFxP::lit("0x1.123"), USampleFxP::lit("0x1.135"), USampleFxP::lit("0x1.148"),
+        USampleFxP::lit("0x1.15b"), USampleFxP::lit("0x1.16e"), USampleFxP::lit("0x1.181"), USampleFxP::lit("0x1.194"),
+        USampleFxP::lit("0x1.1a7"), USampleFxP::lit("0x1.1bb"), USampleFxP::lit("0x1.1cf"), USampleFxP::lit("0x1.1e2"),
+        USampleFxP::lit("0x1.1f7"), USampleFxP::lit("0x1.20b"), USampleFxP::lit("0x1.21f"), USampleFxP::lit("0x1.234"),
+        USampleFxP::lit("0x1.249"), USampleFxP::lit("0x1.25e"), USampleFxP::lit("0x1.273"), USampleFxP::lit("0x1.288"),
+        USampleFxP::lit("0x1.29e"), USampleFxP::lit("0x1.2b4"), USampleFxP::lit("0x1.2c9"), USampleFxP::lit("0x1.2e0"),
+        USampleFxP::lit("0x1.2f6"), USampleFxP::lit("0x1.30d"), USampleFxP::lit("0x1.323"), USampleFxP::lit("0x1.33a"),
+        USampleFxP::lit("0x1.352"), USampleFxP::lit("0x1.369"), USampleFxP::lit("0x1.381"), USampleFxP::lit("0x1.399"),
+        USampleFxP::lit("0x1.3b1"), USampleFxP::lit("0x1.3c9"), USampleFxP::lit("0x1.3e2"), USampleFxP::lit("0x1.3fb"),
+        USampleFxP::lit("0x1.414"), USampleFxP::lit("0x1.42d"), USampleFxP::lit("0x1.446"), USampleFxP::lit("0x1.460"),
+        USampleFxP::lit("0x1.47a"), USampleFxP::lit("0x1.495"), USampleFxP::lit("0x1.4af"), USampleFxP::lit("0x1.4ca"),
+        USampleFxP::lit("0x1.4e5"), USampleFxP::lit("0x1.501"), USampleFxP::lit("0x1.51d"), USampleFxP::lit("0x1.539"),
+        USampleFxP::lit("0x1.555"), USampleFxP::lit("0x1.571"), USampleFxP::lit("0x1.58e"), USampleFxP::lit("0x1.5ac"),
+        USampleFxP::lit("0x1.5c9"), USampleFxP::lit("0x1.5e7"), USampleFxP::lit("0x1.605"), USampleFxP::lit("0x1.623"),
+        USampleFxP::lit("0x1.642"), USampleFxP::lit("0x1.661"), USampleFxP::lit("0x1.681"), USampleFxP::lit("0x1.6a1"),
+        USampleFxP::lit("0x1.6c1"), USampleFxP::lit("0x1.6e1"), USampleFxP::lit("0x1.702"), USampleFxP::lit("0x1.724"),
+        USampleFxP::lit("0x1.745"), USampleFxP::lit("0x1.767"), USampleFxP::lit("0x1.78a"), USampleFxP::lit("0x1.7ad"),
+        USampleFxP::lit("0x1.7d0"), USampleFxP::lit("0x1.7f4"), USampleFxP::lit("0x1.818"), USampleFxP::lit("0x1.83c"),
+        USampleFxP::lit("0x1.861"), USampleFxP::lit("0x1.886"), USampleFxP::lit("0x1.8ac"), USampleFxP::lit("0x1.8d3"),
+        USampleFxP::lit("0x1.8f9"), USampleFxP::lit("0x1.920"), USampleFxP::lit("0x1.948"), USampleFxP::lit("0x1.970"),
+        USampleFxP::lit("0x1.999"), USampleFxP::lit("0x1.9c2"), USampleFxP::lit("0x1.9ec"), USampleFxP::lit("0x1.a16"),
+        USampleFxP::lit("0x1.a41"), USampleFxP::lit("0x1.a6d"), USampleFxP::lit("0x1.a98"), USampleFxP::lit("0x1.ac5"),
+        USampleFxP::lit("0x1.af2"), USampleFxP::lit("0x1.b20"), USampleFxP::lit("0x1.b4e"), USampleFxP::lit("0x1.b7d"),
+        USampleFxP::lit("0x1.bac"), USampleFxP::lit("0x1.bdd"), USampleFxP::lit("0x1.c0e"), USampleFxP::lit("0x1.c3f"),
+        USampleFxP::lit("0x1.c71"), USampleFxP::lit("0x1.ca4"), USampleFxP::lit("0x1.cd8"), USampleFxP::lit("0x1.d0c"),
+        USampleFxP::lit("0x1.d41"), USampleFxP::lit("0x1.d77"), USampleFxP::lit("0x1.dae"), USampleFxP::lit("0x1.de5"),
+        USampleFxP::lit("0x1.e1e"), USampleFxP::lit("0x1.e57"), USampleFxP::lit("0x1.e91"), USampleFxP::lit("0x1.ecc"),
+        USampleFxP::lit("0x1.f07"), USampleFxP::lit("0x1.f44"), USampleFxP::lit("0x1.f81"), USampleFxP::lit("0x1.fc0"),
+        USampleFxP::lit("0x2.000"), USampleFxP::lit("0x2.040"), USampleFxP::lit("0x2.082"), USampleFxP::lit("0x2.0c4"),
+        USampleFxP::lit("0x2.108"), USampleFxP::lit("0x2.14d"), USampleFxP::lit("0x2.192"), USampleFxP::lit("0x2.1d9"),
+        USampleFxP::lit("0x2.222"), USampleFxP::lit("0x2.26b"), USampleFxP::lit("0x2.2b6"), USampleFxP::lit("0x2.302"),
+        USampleFxP::lit("0x2.34f"), USampleFxP::lit("0x2.39e"), USampleFxP::lit("0x2.3ee"), USampleFxP::lit("0x2.43f"),
+        USampleFxP::lit("0x2.492"), USampleFxP::lit("0x2.4e6"), USampleFxP::lit("0x2.53c"), USampleFxP::lit("0x2.593"),
+        USampleFxP::lit("0x2.5ed"), USampleFxP::lit("0x2.647"), USampleFxP::lit("0x2.6a4"), USampleFxP::lit("0x2.702"),
+        USampleFxP::lit("0x2.762"), USampleFxP::lit("0x2.7c4"), USampleFxP::lit("0x2.828"), USampleFxP::lit("0x2.88d"),
+        USampleFxP::lit("0x2.8f5"), USampleFxP::lit("0x2.95f"), USampleFxP::lit("0x2.9cb"), USampleFxP::lit("0x2.a3a"),
+        USampleFxP::lit("0x2.aaa"), USampleFxP::lit("0x2.b1d"), USampleFxP::lit("0x2.b93"), USampleFxP::lit("0x2.c0b"),
+        USampleFxP::lit("0x2.c85"), USampleFxP::lit("0x2.d02"), USampleFxP::lit("0x2.d82"), USampleFxP::lit("0x2.e05"),
+        USampleFxP::lit("0x2.e8b"), USampleFxP::lit("0x2.f14"), USampleFxP::lit("0x2.fa0"), USampleFxP::lit("0x3.030"),
+        USampleFxP::lit("0x3.0c3"), USampleFxP::lit("0x3.159"), USampleFxP::lit("0x3.1f3"), USampleFxP::lit("0x3.291"),
+        USampleFxP::lit("0x3.333"), USampleFxP::lit("0x3.3d9"), USampleFxP::lit("0x3.483"), USampleFxP::lit("0x3.531"),
+        USampleFxP::lit("0x3.5e5"), USampleFxP::lit("0x3.69d"), USampleFxP::lit("0x3.759"), USampleFxP::lit("0x3.81c"),
+        USampleFxP::lit("0x3.8e3"), USampleFxP::lit("0x3.9b0"), USampleFxP::lit("0x3.a83"), USampleFxP::lit("0x3.b5c"),
+        USampleFxP::lit("0x3.c3c"), USampleFxP::lit("0x3.d22"), USampleFxP::lit("0x3.e0f"), USampleFxP::lit("0x3.f03"),
+        USampleFxP::lit("0x4.000"), USampleFxP::lit("0x4.104"), USampleFxP::lit("0x4.210"), USampleFxP::lit("0x4.325"),
+        USampleFxP::lit("0x4.444"), USampleFxP::lit("0x4.56c"), USampleFxP::lit("0x4.69e"), USampleFxP::lit("0x4.7dc"),
+        USampleFxP::lit("0x4.924"), USampleFxP::lit("0x4.a79"), USampleFxP::lit("0x4.bda"), USampleFxP::lit("0x4.d48"),
+        USampleFxP::lit("0x4.ec4"), USampleFxP::lit("0x5.050"), USampleFxP::lit("0x5.1eb"), USampleFxP::lit("0x5.397"),
+        USampleFxP::lit("0x5.555"), USampleFxP::lit("0x5.726"), USampleFxP::lit("0x5.90b"), USampleFxP::lit("0x5.b05"),
+        USampleFxP::lit("0x5.d17"), USampleFxP::lit("0x5.f41"), USampleFxP::lit("0x6.186"), USampleFxP::lit("0x6.3e7"),
+        USampleFxP::lit("0x6.666"), USampleFxP::lit("0x6.906"), USampleFxP::lit("0x6.bca"), USampleFxP::lit("0x6.eb3"),
+        USampleFxP::lit("0x7.1c7"), USampleFxP::lit("0x7.507"), USampleFxP::lit("0x7.878"), USampleFxP::lit("0x7.c1f"),
+        USampleFxP::lit("0x8.000"), USampleFxP::lit("0x8.421"), USampleFxP::lit("0x8.888"), USampleFxP::lit("0x8.d3d"),
+        USampleFxP::lit("0x9.249"), USampleFxP::lit("0x9.7b4"), USampleFxP::lit("0x9.d89"), USampleFxP::lit("0xa.3d7"),
+        USampleFxP::lit("0xa.aaa"), USampleFxP::lit("0xb.216"), USampleFxP::lit("0xb.a2e"), USampleFxP::lit("0xc.30c"),
+        USampleFxP::lit("0xc.ccc"), USampleFxP::lit("0xd.794"), USampleFxP::lit("0xe.38e"), USampleFxP::lit("0xf.0f0"),
+        USampleFxP::lit("0xf.fff"), USampleFxP::lit("0xf.fff")]; //throw 2x maxs at the end to avoid out-of-bounds on CLIP_MAX
+    let index = x_bits >> 8;
+    let lookup_val = LOOKUP_TABLE[index as usize];
+    let interp = (LOOKUP_TABLE[index as usize + 1] - lookup_val)
+        .wide_mul(fixedmath::U8F8::from_bits(x_bits & 0xFF));
+    lookup_val + USampleFxP::from_num(interp)
+}
+
+fn one_over_one_plus_x(x_arg: ScalarFxP) -> fixedmath::U1F15 {
+    if x_arg == ScalarFxP::ZERO {
+        return fixedmath::U1F15::ONE;
+    }
+    // Need to match the clipping behavior of the other one_over_one_minus_x():
+    let x = fixedmath::I1F15::from_num(clip_shape(x_arg));
+    let x_prime = x - fixedmath::I1F15::lit("0.333333333");
+    let mut acc = fixedmath::I2F14::ONE;
+    for _ in 0..4 {
+        let mut prod = fixedmath::I16F16::from_num(acc.wide_mul(x_prime));
+        prod += prod.unwrapped_shl(1); // acc *= 3
+        prod = prod.unwrapped_shr(2);  // acc /= 4
+        acc = fixedmath::I2F14::ONE - fixedmath::I2F14::from_num(prod);
+    }
+    acc = acc.unwrapped_shr(2);  // acc / 4
+    acc += acc.unwrapped_shl(1); // acc *= 3
+    fixedmath::U1F15::from_num(acc)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn shape_mod_calculation() {
+        let mut rms_error_pos = 0f32;
+        let mut rms_error_neg = 0f32;
+        for i in 0..1024 {
+            let x_wide = fixedmath::U16F16::from_num(i).unwrapped_shr(10);
+            let x = clip_shape(ScalarFxP::from_num(x_wide));
+            let xf = x.to_num::<f32>();
+            let x_pos = one_over_one_plus_x(x);
+            let xf_pos = 1f32 / (1f32 + xf);
+            let x_neg = one_over_one_minus_x(x);
+            let xf_neg = 1f32 / (1f32 - xf);
+            let error_pos = (x_pos.to_num::<f32>() / xf_pos) - 1f32;
+            let error_neg = (x_neg.to_num::<f32>() / xf_neg) - 1f32;
+            rms_error_pos += error_pos*error_pos;
+            rms_error_neg += error_neg*error_neg;
+        }
+        rms_error_pos = (rms_error_pos / 1024f32).sqrt();
+        rms_error_neg = (rms_error_neg / 1024f32).sqrt();
+        assert!(rms_error_pos < 0.01f32); //FIXME: Should probably have better thresholds
+        assert!(rms_error_neg < 0.01f32);
+    }
+    #[test]
+    fn shape_mod_nopanic() {
+        for i in 0..(1<<16) {
+            let x = ScalarFxP::from_bits(i as u16);
+            let _pos = one_over_one_plus_x(x);
+            let _neg = one_over_one_minus_x(x);
+        }
     }
 }
 
