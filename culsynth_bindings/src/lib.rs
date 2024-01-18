503d79b0cd1e1f00c9e0a46adad0e756cf469d70
rbmj <rbmj@verizon.net>
2024-01-18 13:35:04 -0800

    Fix bindings w/ new API

--- culsynth_bindings/src/lib.rs
+++ culsynth_bindings/src/lib.rs
@@ -1,47 +1,98 @@
+#![no_std]
+
+extern crate alloc;
+use alloc::boxed::Box;
+
 use culsynth::context::{Context, ContextFxP};
 use culsynth::devices::*;
 use culsynth::{EnvParamFxP, NoteFxP, SampleFxP, ScalarFxP, SignedNoteFxP};
 
+use core::iter::zip;
+
+struct PtrIterator<T> {
+    data: *mut T,
+}
+
+impl<T> PtrIterator<T> {
+    unsafe fn new(data: *mut T) -> Self {
+        Self { data }
+    }
+}
+
+// This blows a gigantic hole through the borrow checker.  Only use this local
+// to a specific function otherwise memory leaks are *bound* to happen
+// TODO:  Make this less of a footgun
+impl<T: 'static> Iterator for PtrIterator<T> {
+    type Item = &'static mut T;
+    fn next(&mut self) -> Option<&'static mut T> {
+        unsafe {
+            let ret = &mut *(self.data);
+            self.data = self.data.add(1);
+            Some(ret)
+        }
+    }
+}
+
+const SR_480_VAL: u32 = 0;
+const SR_441_VAL: u32 = 1;
+
 #[no_mangle]
-pub extern "C" fn culsynth_amp_u16_new() -> *mut AmpFxP {
-    Box::into_raw(Box::new(AmpFxP::new()))
+pub static CULSYNTH_SR_480: u32 = SR_480_VAL;
+#[no_mangle]
+pub static CULSYNTH_SR_441: u32 = SR_441_VAL;
+
+fn contextfxp_from_u32(sr: u32) -> Option<ContextFxP> {
+    match sr {
+        SR_480_VAL => Some(ContextFxP::new_480()),
+        SR_441_VAL => Some(ContextFxP::new_441()),
+        _ => None,
+    }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn culsynth_amp_u16_free(p: *mut AmpFxP) {
+pub extern "C" fn culsynth_amp_i16_new() -> *mut Amp<i16> {
+    Box::into_raw(Box::new(Amp::<i16>::default()))
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn culsynth_amp_i16_free(p: *mut Amp<i16>) {
     if !p.is_null() {
         let _ = Box::from_raw(p);
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn culsynth_amp_u16_process(
-    p: *mut AmpFxP,
+pub unsafe extern "C" fn culsynth_amp_i16_process(
+    p: *mut Amp<i16>,
+    sr: u32,
     samples: u32,
     signal: *const i16,
-    gain: *const i16,
-    out: *mut *const u16,
-    offset: u32,
+    gain: *const u16,
+    out: *mut i16,
 ) -> i32 {
     if p.is_null() || signal.is_null() || gain.is_null() || out.is_null() {
         return -1;
     }
-    let s = std::slice::from_raw_parts(
-        signal.offset(offset as isize).cast::<SampleFxP>(),
-        samples as usize,
-    );
-    let g = std::slice::from_raw_parts(
-        gain.offset(offset as isize).cast::<SampleFxP>(),
-        samples as usize,
-    );
-    let out_slice = (*p).process(s, g);
-    *out = out_slice.as_ptr().cast();
-    out_slice.len() as i32
+    let context = match contextfxp_from_u32(sr) {
+        Some(x) => x,
+        None => return -1,
+    };
+    let s = core::slice::from_raw_parts(signal.cast::<SampleFxP>(), samples as usize);
+    let g = core::slice::from_raw_parts(gain.cast::<ScalarFxP>(), samples as usize);
+    let mut processed = 0i32;
+    for (o, smp) in zip(
+        PtrIterator::new(out),
+        (*p).process(&context, s.iter().copied(), g.into_iter().copied()),
+    ) {
+        *o = smp.to_bits();
+        processed += 1;
+    }
+    processed
 }
 
 #[no_mangle]
 pub extern "C" fn culsynth_amp_f32_new() -> *mut Amp<f32> {
-    Box::into_raw(Box::new(Amp::new()))
+    Box::into_raw(Box::new(Amp::default()))
 }
 
 #[no_mangle]
@@ -54,45 +105,51 @@ pub unsafe extern "C" fn culsynth_amp_f32_free(p: *mut Amp<f32>) {
 #[no_mangle]
 pub unsafe extern "C" fn culsynth_amp_f32_process(
     p: *mut Amp<f32>,
+    sr: f32,
     samples: u32,
     signal: *const f32,
     gain: *const f32,
-    out: *mut *const f32,
-    offset: u32,
+    out: *mut f32,
 ) -> i32 {
     if p.is_null() || signal.is_null() || gain.is_null() || out.is_null() {
         return -1;
     }
-    let s = std::slice::from_raw_parts(signal.offset(offset as isize), samples as usize);
-    let g = std::slice::from_raw_parts(gain.offset(offset as isize), samples as usize);
-    let out_slice = (*p).process(s, g);
-    *out = out_slice.as_ptr().cast();
-    out_slice.len() as i32
+    let s = core::slice::from_raw_parts(signal, samples as usize);
+    let g = core::slice::from_raw_parts(gain, samples as usize);
+    let mut processed = 0i32;
+    for (o, smp) in zip(
+        PtrIterator::new(out),
+        (*p).process(&Context::new(sr), s.iter().copied(), g.iter().copied()),
+    ) {
+        *o = smp;
+        processed += 1;
+    }
+    processed
 }
 
 #[no_mangle]
-pub extern "C" fn culsynth_env_u16_new() -> *mut EnvFxP {
-    Box::into_raw(Box::new(EnvFxP::new()))
+pub extern "C" fn culsynth_env_i16_new() -> *mut Env<i16> {
+    Box::into_raw(Box::new(Env::<i16>::default()))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn culsynth_env_u16_free(p: *mut EnvFxP) {
+pub unsafe extern "C" fn culsynth_env_i16_free(p: *mut Env<i16>) {
     if !p.is_null() {
         let _ = Box::from_raw(p);
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn culsynth_env_u16_process(
-    p: *mut EnvFxP,
+pub unsafe extern "C" fn culsynth_env_i16_process(
+    p: *mut Env<i16>,
+    sr: u32,
     samples: u32,
     gate: *const i16,
     attack: *const u16,
     decay: *const u16,
     sustain: *const u16,
     release: *const u16,
-    signal: *mut *const u16,
-    offset: u32,
+    signal: *mut u16,
 ) -> i32 {
     if p.is_null()
         || gate.is_null()
@@ -104,41 +161,32 @@ pub unsafe extern "C" fn culsynth_env_u16_process(
     {
         return -1;
     }
-    let g = std::slice::from_raw_parts(
-        gate.offset(offset as isize).cast::<SampleFxP>(),
-        samples as usize,
-    );
-    let a = std::slice::from_raw_parts(
-        attack.offset(offset as isize).cast::<EnvParamFxP>(),
-        samples as usize,
-    );
-    let d = std::slice::from_raw_parts(
-        decay.offset(offset as isize).cast::<EnvParamFxP>(),
-        samples as usize,
-    );
-    let s = std::slice::from_raw_parts(
-        sustain.offset(offset as isize).cast::<ScalarFxP>(),
-        samples as usize,
-    );
-    let r = std::slice::from_raw_parts(
-        release.offset(offset as isize).cast::<EnvParamFxP>(),
-        samples as usize,
-    );
-    let params = EnvParamsFxP {
-        attack: a,
-        decay: d,
-        sustain: s,
-        release: r,
+    let context = match contextfxp_from_u32(sr) {
+        Some(x) => x,
+        None => return -1,
     };
-    let ctx = ContextFxP::default();
-    let out = (*p).process(&ctx, g, params);
-    *signal = out.as_ptr().cast();
-    out.len() as i32
+    let g = core::slice::from_raw_parts(gate.cast::<SampleFxP>(), samples as usize);
+    let a = core::slice::from_raw_parts(attack.cast::<EnvParamFxP>(), samples as usize);
+    let d = core::slice::from_raw_parts(decay.cast::<EnvParamFxP>(), samples as usize);
+    let s = core::slice::from_raw_parts(sustain.cast::<ScalarFxP>(), samples as usize);
+    let r = core::slice::from_raw_parts(release.cast::<EnvParamFxP>(), samples as usize);
+    let paramiter = new_env_param_iter()
+        .with_attack(a.iter().copied())
+        .with_decay(d.iter().copied())
+        .with_sustain(s.iter().copied())
+        .with_release(r.iter().copied());
+    let out = (*p).process(&context, g.iter().copied(), paramiter);
+    let mut processed = 0i32;
+    for (o, smp) in zip(PtrIterator::new(signal), out) {
+        *o = smp.to_bits();
+        processed += 1;
+    }
+    processed
 }
 
 #[no_mangle]
 pub extern "C" fn culsynth_env_f32_new() -> *mut Env<f32> {
-    Box::into_raw(Box::new(Env::new()))
+    Box::into_raw(Box::new(Env::default()))
 }
 
 #[no_mangle]
@@ -151,14 +199,14 @@ pub unsafe extern "C" fn culsynth_env_f32_free(p: *mut Env<f32>) {
 #[no_mangle]
 pub unsafe extern "C" fn culsynth_env_f32_process(
     p: *mut Env<f32>,
+    sr: f32,
     samples: u32,
     gate: *const f32,
     attack: *const f32,
     decay: *const f32,
     sustain: *const f32,
     release: *const f32,
-    signal: *mut *const f32,
-    offset: u32,
+    signal: *mut f32,
 ) -> i32 {
     if p.is_null()
         || gate.is_null()
@@ -170,49 +218,49 @@ pub unsafe extern "C" fn culsynth_env_f32_process(
     {
         return -1;
     }
-    let g = std::slice::from_raw_parts(gate.offset(offset as isize), samples as usize);
-    let a = std::slice::from_raw_parts(attack.offset(offset as isize), samples as usize);
-    let d = std::slice::from_raw_parts(decay.offset(offset as isize), samples as usize);
-    let s = std::slice::from_raw_parts(sustain.offset(offset as isize), samples as usize);
-    let r = std::slice::from_raw_parts(release.offset(offset as isize), samples as usize);
-    let params = EnvParams::<f32> {
-        attack: a,
-        decay: d,
-        sustain: s,
-        release: r,
-    };
-    //FIXME
-    let ctx = Context::<f32> {
-        sample_rate: 44100f32,
-    };
-    let out = (*p).process(&ctx, g, params);
-    *signal = out.as_ptr().cast();
-    out.len() as i32
+    let g = core::slice::from_raw_parts(gate, samples as usize);
+    let a = core::slice::from_raw_parts(attack, samples as usize);
+    let d = core::slice::from_raw_parts(decay, samples as usize);
+    let s = core::slice::from_raw_parts(sustain, samples as usize);
+    let r = core::slice::from_raw_parts(release, samples as usize);
+    let paramiter = new_env_param_iter()
+        .with_attack(a.iter().copied())
+        .with_decay(d.iter().copied())
+        .with_sustain(s.iter().copied())
+        .with_release(r.iter().copied());
+    let ctx = Context::<f32> { sample_rate: sr };
+    let out = (*p).process(&ctx, g.iter().copied(), paramiter);
+    let mut processed = 0i32;
+    for (o, smp) in zip(PtrIterator::new(signal), out) {
+        *o = smp;
+        processed += 1;
+    }
+    processed
 }
 
 #[no_mangle]
-pub extern "C" fn culsynth_filt_u16_new() -> *mut FiltFxP {
-    Box::into_raw(Box::new(FiltFxP::new()))
+pub extern "C" fn culsynth_filt_i16_new() -> *mut Filt<i16> {
+    Box::into_raw(Box::new(Filt::new()))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn culsynth_filt_u16_free(p: *mut FiltFxP) {
+pub unsafe extern "C" fn culsynth_filt_i16_free(p: *mut Filt<i16>) {
     if !p.is_null() {
         let _ = Box::from_raw(p);
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn culsynth_filt_u16_process(
-    p: *mut FiltFxP,
+pub unsafe extern "C" fn culsynth_filt_i16_process(
+    p: *mut Filt<i16>,
+    sr: u32,
     samples: u32,
     input: *const i16,
     cutoff: *const u16,
     resonance: *const u16,
-    low: *mut *const i16,
-    band: *mut *const i16,
-    high: *mut *const i16,
-    offset: u32,
+    low: *mut i16,
+    band: *mut i16,
+    high: *mut i16,
 ) -> i32 {
     if p.is_null()
         || input.is_null()
@@ -224,29 +272,28 @@ pub unsafe extern "C" fn culsynth_filt_u16_process(
     {
         return -1;
     }
-    let i = std::slice::from_raw_parts(
-        input.offset(offset as isize).cast::<SampleFxP>(),
-        samples as usize,
-    );
-    let c = std::slice::from_raw_parts(
-        cutoff.offset(offset as isize).cast::<NoteFxP>(),
-        samples as usize,
-    );
-    let r = std::slice::from_raw_parts(
-        resonance.offset(offset as isize).cast::<ScalarFxP>(),
-        samples as usize,
-    );
-    let params = FiltParamsFxP {
-        cutoff: c,
-        resonance: r,
+    let low = PtrIterator::new(low);
+    let band = PtrIterator::new(band);
+    let high = PtrIterator::new(high);
+    let ctx = match contextfxp_from_u32(sr) {
+        Some(x) => x,
+        None => return -1,
     };
-    //FIXME
-    let ctx = ContextFxP::default();
-    let out = (*p).process(&ctx, i, params);
-    *low = out.low.as_ptr().cast();
-    *band = out.band.as_ptr().cast();
-    *high = out.high.as_ptr().cast();
-    out.low.len() as i32
+    let i = core::slice::from_raw_parts(input.cast::<SampleFxP>(), samples as usize);
+    let c = core::slice::from_raw_parts(cutoff.cast::<NoteFxP>(), samples as usize);
+    let r = core::slice::from_raw_parts(resonance.cast::<ScalarFxP>(), samples as usize);
+    let params = new_filt_param_iter()
+        .with_cutoff(c.iter().copied())
+        .with_resonance(r.iter().copied());
+    let out = (*p).process(&ctx, i.iter().copied(), params);
+    let mut processed = 0i32;
+    for (l, (b, (h, o))) in zip(low, zip(band, zip(high, out))) {
+        *l = o.low.to_bits();
+        *b = o.band.to_bits();
+        *h = o.high.to_bits();
+        processed += 1;
+    }
+    processed
 }
 
 #[no_mangle]
@@ -264,14 +311,14 @@ pub unsafe extern "C" fn culsynth_filt_f32_free(p: *mut Filt<f32>) {
 #[no_mangle]
 pub unsafe extern "C" fn culsynth_filt_f32_process(
     p: *mut Filt<f32>,
+    sr: f32,
     samples: u32,
     input: *const f32,
     cutoff: *const f32,
     resonance: *const f32,
-    low: *mut *const f32,
-    band: *mut *const f32,
-    high: *mut *const f32,
-    offset: u32,
+    low: *mut f32,
+    band: *mut f32,
+    high: *mut f32,
 ) -> i32 {
     if p.is_null()
         || input.is_null()
@@ -283,48 +330,53 @@ pub unsafe extern "C" fn culsynth_filt_f32_process(
     {
         return -1;
     }
-    let i = std::slice::from_raw_parts(input.offset(offset as isize), samples as usize);
-    let c = std::slice::from_raw_parts(cutoff.offset(offset as isize), samples as usize);
-    let r = std::slice::from_raw_parts(resonance.offset(offset as isize), samples as usize);
-    let params = FiltParams::<f32> {
-        cutoff: c,
-        resonance: r,
-    };
-    //FIXME
+    let low = PtrIterator::new(low);
+    let band = PtrIterator::new(band);
+    let high = PtrIterator::new(high);
+    let i = core::slice::from_raw_parts(input, samples as usize);
+    let c = core::slice::from_raw_parts(cutoff, samples as usize);
+    let r = core::slice::from_raw_parts(resonance, samples as usize);
+    let params = new_filt_param_iter()
+        .with_cutoff(c.iter().copied())
+        .with_resonance(r.iter().copied());
     let ctx = Context::<f32> {
-        sample_rate: 44100f32,
+        sample_rate: sr,
     };
-    let out = (*p).process(&ctx, i, params);
-    *low = out.low.as_ptr().cast();
-    *band = out.band.as_ptr().cast();
-    *high = out.high.as_ptr().cast();
-    out.low.len() as i32
+    let out = (*p).process(&ctx, i.iter().copied(), params);
+    let mut processed = 0i32;
+    for (l, (b, (h, o))) in zip(low, zip(band, zip(high, out))) {
+        *l = o.low;
+        *b = o.band;
+        *h = o.high;
+        processed += 1;
+    }
+    processed
 }
 
 #[no_mangle]
-pub extern "C" fn culsynth_osc_u16_new() -> *mut OscFxP {
-    Box::into_raw(Box::new(OscFxP::new()))
+pub extern "C" fn culsynth_osc_i16_new() -> *mut Osc<i16> {
+    Box::into_raw(Box::new(Osc::new()))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn culsynth_osc_u16_free(p: *mut OscFxP) {
+pub unsafe extern "C" fn culsynth_osc_i16_free(p: *mut Osc<i16>) {
     if !p.is_null() {
         let _ = Box::from_raw(p);
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn culsynth_osc_u16_process(
-    p: *mut OscFxP,
+pub unsafe extern "C" fn culsynth_osc_i16_process(
+    p: *mut Osc<i16>,
+    sr: u32,
     samples: u32,
     note: *const u16,
     tune: *const i16,
     shape: *const u16,
-    sin: *mut *const i16,
-    tri: *mut *const i16,
-    sq: *mut *const i16,
-    saw: *mut *const i16,
-    offset: u32,
+    sin: *mut i16,
+    tri: *mut i16,
+    sq: *mut i16,
+    saw: *mut i16,
 ) -> i32 {
     if p.is_null()
         || tune.is_null()
@@ -336,31 +388,39 @@ pub unsafe extern "C" fn culsynth_osc_u16_process(
     {
         return -1;
     }
-    let note_s = std::slice::from_raw_parts(
-        note.offset(offset as isize).cast::<NoteFxP>(),
-        samples as usize,
-    );
-    let shape_s = std::slice::from_raw_parts(
-        shape.offset(offset as isize).cast::<ScalarFxP>(),
-        samples as usize,
-    );
-    let tune_s = std::slice::from_raw_parts(
-        tune.offset(offset as isize).cast::<SignedNoteFxP>(),
-        samples as usize,
-    );
-    let params = OscParamsFxP {
-        tune: tune_s,
-        shape: shape_s,
-        sync: OscSync::Off,
+    let sin = PtrIterator::new(sin);
+    let tri = PtrIterator::new(tri);
+    let sq = PtrIterator::new(sq);
+    let saw = PtrIterator::new(saw);
+    let ctx = match contextfxp_from_u32(sr) {
+        Some(x) => x,
+        None => return -1,
     };
-    //FIXME
-    let ctx = ContextFxP::default();
-    let out = (*p).process(&ctx, note_s, params);
-    *sin = out.sin.as_ptr().cast();
-    *tri = out.tri.as_ptr().cast();
-    *sq = out.sq.as_ptr().cast();
-    *saw = out.saw.as_ptr().cast();
-    out.sin.len() as i32
+    let note_s = core::slice::from_raw_parts(
+        note.cast::<NoteFxP>(),
+        samples as usize,
+    );
+    let shape_s = core::slice::from_raw_parts(
+        shape.cast::<ScalarFxP>(),
+        samples as usize,
+    );
+    let tune_s = core::slice::from_raw_parts(
+        tune.cast::<SignedNoteFxP>(),
+        samples as usize,
+    );
+    let params = new_osc_param_iter()
+        .with_tune(tune_s.iter().copied())
+        .with_shape(shape_s.iter().copied());
+    let out = (*p).process(&ctx, note_s.iter().copied(), params);
+    let mut processed = 0i32;
+    for (n, (t, (q, (s, o)))) in zip(sin, zip(tri, zip(sq, zip(saw, out)))) {
+        *n = o.sin.to_bits();
+        *t = o.tri.to_bits();
+        *q = o.sq.to_bits();
+        *s = o.saw.to_bits();
+        processed += 1;
+    }
+    processed
 }
 
 #[no_mangle]
@@ -378,15 +438,15 @@ pub unsafe extern "C" fn culsynth_osc_f32_free(p: *mut Osc<f32>) {
 #[no_mangle]
 pub unsafe extern "C" fn culsynth_osc_f32_process(
     p: *mut Osc<f32>,
+    sr: f32,
     samples: u32,
     note: *const f32,
     tune: *const f32,
     shape: *const f32,
-    sin: *mut *const f32,
-    tri: *mut *const f32,
-    sq: *mut *const f32,
-    saw: *mut *const f32,
-    offset: u32,
+    sin: *mut f32,
+    tri: *mut f32,
+    sq: *mut f32,
+    saw: *mut f32,
 ) -> i32 {
     if p.is_null()
         || tune.is_null()
@@ -399,22 +459,27 @@ pub unsafe extern "C" fn culsynth_osc_f32_process(
     {
         return -1;
     }
-    let note_s = std::slice::from_raw_parts(note.offset(offset as isize), samples as usize);
-    let shape_s = std::slice::from_raw_parts(shape.offset(offset as isize), samples as usize);
-    let tune_s = std::slice::from_raw_parts(tune.offset(offset as isize), samples as usize);
-    let params = OscParams::<f32> {
-        tune: tune_s,
-        shape: shape_s,
-        sync: OscSync::Off,
-    };
-    //FIXME
+    let sin = PtrIterator::new(sin);
+    let tri = PtrIterator::new(tri);
+    let sq = PtrIterator::new(sq);
+    let saw = PtrIterator::new(saw);
+    let note_s = core::slice::from_raw_parts(note, samples as usize);
+    let shape_s = core::slice::from_raw_parts(shape, samples as usize);
+    let tune_s = core::slice::from_raw_parts(tune, samples as usize);
+    let params = new_osc_param_iter()
+        .with_tune(tune_s.iter().copied())
+        .with_shape(shape_s.iter().copied());
     let ctx = Context::<f32> {
-        sample_rate: 44100f32,
+        sample_rate: sr,
     };
-    let out = (*p).process(&ctx, note_s, params);
-    *sin = out.sin.as_ptr().cast();
-    *tri = out.tri.as_ptr().cast();
-    *sq = out.sq.as_ptr().cast();
-    *saw = out.saw.as_ptr().cast();
-    out.sin.len() as i32
+    let out = (*p).process(&ctx, note_s.iter().copied(), params);
+    let mut processed = 0i32;
+    for (n, (t, (q, (s, o)))) in zip(sin, zip(tri, zip(sq, zip(saw, out)))) {
+        *n = o.sin;
+        *t = o.tri;
+        *q = o.sq;
+        *s = o.saw;
+        processed += 1;
+    }
+    processed
 }
